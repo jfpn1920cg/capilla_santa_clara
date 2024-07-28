0f42c51b412099d8352d1492d1eddb9dbfabfdca
jfpn1920cr <162055100+jfpn1920cg@users.noreply.github.com>
2024-07-28 18:31:01 -0500

    ajustando_contenido

--- Inicio_de_sesion.php
+++ Inicio_de_sesion.php
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['username'] = $user['usuario'];
-        header("Location: panel_de_control.php");
+        header("Location: Panel_de_control.php");
         exit();
     } else {
         echo "Usuario o contraseña incorrectos.";
